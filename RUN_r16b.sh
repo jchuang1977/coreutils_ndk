600d9630bfe9b274e3ef1563cb3d36234cdd3480
andy.huang <andy.huang@wnc.com.tw>
2018-09-20 14:30:41 +0800

    add run script for r16b

--- RUN_r16b.sh
+++ RUN_r16b.sh
@@ -8,12 +8,12 @@
 export PATH="$NDK/toolchains//aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/:$PATH"
 export SYS_ROOT="$NDK/sysroot/"
 export CC="aarch64-linux-android-gcc --sysroot=$SYS_ROOT"
-#export CC="aarch64-linux-android-gcc"
+export CC="aarch64-linux-android-gcc"
 export LD="aarch64-linux-android-ld"
 export AR="aarch64-linux-android-ar"
 export RANLIB="aarch64-linux-android-ranlib"
 export STRIP="aarch64-linux-android-strip"
 
-./configure --host=aarch64  LIBS="-lc -lgcc" CFLAGS="-Wno-implicit-function-declaration -Wno-int-to-pointer-cast -pie -fPIE"
+./configure --host=aarch64 CFLAGS="-Wno-implicit-function-declaration -Wno-int-to-pointer-cast -pie -fPIE"
 
 make
